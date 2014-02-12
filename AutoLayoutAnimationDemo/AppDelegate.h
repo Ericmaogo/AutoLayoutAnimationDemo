ffa6da7e5c8d197433bf87cd9ab400584a826234
Jens Schwarzer <schwarzer@schwarzer.dk>
2014-02-12 12:27:36 +0100

    Initial Commit

---
+++ AutoLayoutAnimationDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AutoLayoutAnimationDemo
+//
+//  Created by Jens Schwarzer on 12/02/14.
+//  Copyright (c) 2014 marchv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AutoLayoutAnimationDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AutoLayoutAnimationDemo
+//
+//  Created by Jens Schwarzer on 12/02/14.
+//  Copyright (c) 2014 marchv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
